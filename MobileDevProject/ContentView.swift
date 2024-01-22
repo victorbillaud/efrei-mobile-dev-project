7ccb3dec60342b3c7997a15ceb3e2cd0e9825f6f
victorbillaud <victorbillaud@gmail.com>
2024-01-22 14:19:13 +0100

    Initial Commit

---
+++ MobileDevProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileDevProject
+//
+//  Created by Victor BILLAUD on 22/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileDevProject/MobileDevProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevProjectApp.swift
+//  MobileDevProject
+//
+//  Created by Victor BILLAUD on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
